8bf89093ceaaf81928d3a7a3c1aed30c7a2d1e57
Max <hello@mpfglaser.nl>
2023-10-28 15:32:07 +0200

    feat: add basic routing

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,13 @@
+import { Outlet } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
 export function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,39 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Todo from './routes/todo/todo';
+import Home from './routes/home/home';
+import About from './routes/about/about';
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+  {
+    path: 'todo',
+    element: <Todo />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: routes,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
