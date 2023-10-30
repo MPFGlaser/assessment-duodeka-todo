f32360ee0d3a6778db757fa7a1d071fb14821319
Max <hello@mpfglaser.nl>
2023-10-30 09:38:38 +0100

    fix: error in TodoItemCreator.spec.tsx

--- src/components/TodoList/TodoItemCreator/TodoItemCreator.spec.tsx
+++ src/components/TodoList/TodoItemCreator/TodoItemCreator.spec.tsx
@@ -3,8 +3,10 @@ import { render } from '@testing-library/react';
 import TodoItemCreator from './TodoItemCreator';
 
 describe('TodoItemCreator', () => {
+  const onSubmit = vitest.fn();
+
   it('should render successfully', () => {
-    const { baseElement } = render(<TodoItemCreator />);
+    const { baseElement } = render(<TodoItemCreator onSubmit={onSubmit} />);
     expect(baseElement).toBeTruthy();
   });
 });
